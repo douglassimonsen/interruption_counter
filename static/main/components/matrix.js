33fe6bc29075c7b53718ca0d4a090fff3b2b47fa
Matthew Hamilton <mwhamilton6@gmail.com>
2021-06-19 17:31:58 -0500

    Vision has been accomplished

--- static/main/components/matrix.js
+++ static/main/components/matrix.js
@@ -1,5 +1,11 @@
 Vue.component('matrix', {
   props: ['participants', 'interruptions'],
+  data: function(){
+    return {
+      highlightRow: null,
+      hightlightCol: null,
+    };
+  },
   template: `
     <table class="top-list">
       <thead>
@@ -9,25 +15,39 @@ Vue.component('matrix', {
         </tr>
         <tr>
           <td colspan=2></td>
-          <th v-text="p" v-for="p in participants"></th>
+          <th v-text="p" v-for="p, i in participants" style="max-width:60px;" :style="getHeaderStyle(null, i)"></th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="p1, i in participants">
-          <th :rowspan="participants.length" v-if="i === 0" style="max-width:5px"><div style="transform:translate(-40px, 8px) rotate(270deg); width:fit-content;">Interrupting</div></th>
-          <th v-text="p1"></th>
-          <td v-for="p2, j in participants" @click="addInterrupt(i, j)" :style="{backgroundColor: getColor(i, j)}" v-text="getCount(i, j)"></td>
+          <th :rowspan="participants.length" v-if="i === 0" style="max-width:5px;overflow:hidden"><div style="transform:translate(-40px, 8px) rotate(270deg); width:fit-content;">Interrupting</div></th>
+          <th v-text="p1" style="max-width:60px;" :style="getHeaderStyle(i, null)"></th>
+          <td v-for="p2, j in participants" @click="addInterrupt(i, j)" :style="{backgroundColor: getColor(i, j)}" v-text="getCount(i, j)" @mouseover="mousein(i, j)" @mouseout="mouseout(i, j)"></td>
         </tr>
       </tbody>
     </table>
   `,
   methods: {
+    getHeaderStyle: function(row, col){
+      if((row !== null && row === this.highlightRow) || (col !== null && col === this.hightlightCol)){
+        return {backgroundColor: '#FFFFB8', color: '#000000'};
+      }
+      return null;
+    },
     getCount: function(i, j){
       let from = this.participants[i];
       let to = this.participants[j];
       return this.interruptions.reduce(function(a, b){
         return a + +(b.from === from && b.to === to);
       }, 0) || null;
+    },
+    mousein: function(i, j){
+      this.highlightRow = i;
+      this.hightlightCol = j;
+    },
+    mouseout: function(){
+      this.highlightRow = null;
+      this.hightlightCol = null;
     },
     addInterrupt: function(i, j){
       if(i === j){
