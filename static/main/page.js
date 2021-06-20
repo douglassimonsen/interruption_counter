e074b1a883d45ddb783405c676bf00915d3e34ba
Matthew Hamilton <mwhamilton6@gmail.com>
2021-06-20 10:50:42 -0500

    Added localStorage saving of participants, so it doesn't suck so much to reenter.

--- static/main/page.js
+++ static/main/page.js
@@ -2,9 +2,11 @@ new Vue({
   el: '#page',
   data: function(){
     return {
-      participants: ['a', 'b'],
+      participants: [],
       newParticipant: null,
       interruptions: [],
+      savedLists: JSON.parse(localStorage.getItem('savedLists')),
+      selectedList: null,
     };
   },
   mounted: function(){
@@ -20,6 +22,17 @@ new Vue({
     remove: function(removal){
       this.participants = this.participants.filter(x => x !== removal.participant);
       this.interruptions = this.interruptions.filter(x => x.to !== removal.participant && x.from !== removal.participant);
+    },
+    saveList: function(){
+      this.savedLists.push(this.participants);
+      localStorage.setItem('savedLists', JSON.stringify(this.savedLists));
+    },
+    deleteList: function(){
+      this.savedLists.splice(this.selectedList, 1);
+      localStorage.setItem('savedLists', JSON.stringify(this.savedLists));
+    },
+    loadList: function(){
+      this.participants = this.savedLists[this.selectedList];
     }
   },
 });
